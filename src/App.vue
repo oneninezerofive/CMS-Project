35ce7b872fd087d590bd8f982fdc6a736c5c0e28
liuqianghero <1468775781@Qq.com>
2019-08-10 10:52:08 +0800

    yes

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <Administrator />
+    <Addshop />
+  </div>
+</template>
+
+<script>
+import Administrator from './components/qiang/Administrator.vue'
+
+import Addshop from './components/qiang/Addshop.vue'
+export default {
+  name: 'app',
+  components: {
+    Administrator,
+    Addshop
+  }
+}
+</script>
+
+
